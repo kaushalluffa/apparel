944991ec224d503662e99e069dbb4864c2ce09af
kaushalluffa <webdevelopment852@gmail.com>
2022-06-26 14:37:49 -0400

    bug fix not imp

--- src/routes/sign-in/SignIn.jsx
+++ src/routes/sign-in/SignIn.jsx
@@ -1,17 +1,31 @@
-import React from 'react'
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+import {
+  auth,
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+  signInWithGoogleRedirect,
+} from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
-    const logGoogleUser = async () => {
-        const {user} = await signInWithGooglePopup()
-        const userDocRef = await createUserDocumentFromAuth(user)
+  useEffect(async () => {
+    const response = await getRedirectResult(auth);
+    if(response){
+        const userDocRef = await createUserDocumentFromAuth(response.user)
     }
+  }, []);
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+  };
+
   return (
     <div>
-        <h1>sign in page</h1>
-        <button onClick={logGoogleUser}>Sign in with popup</button>
+      <h1>sign in page</h1>
+      <button onClick={logGoogleUser}>Sign in with popup</button>
+      <button onClick={signInWithGoogleRedirect}>Sign in with redirect</button>
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
